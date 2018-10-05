d6ffbd8720c0046b9788d2c4404d88f2ff09efc0
User 1 <user1@rm3302-15s-imac.local>
2018-10-05 13:45:17 -0400

    Initial Commit

---
+++ EcoLetsGo/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  EcoLetsGo
+//
+//  Created by User 1 on 10/5/18.
+//  Copyright © 2018 User 1. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
